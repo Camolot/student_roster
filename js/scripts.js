dbf50010d3bad5fc8b74056be666725bc2fcc425
Camolot <camolotthecreator@gmail.com>
2015-09-19 17:14:29 -0700

    change pizza_thing

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,19 @@
-
+$(document).ready(function() {
+  // $("form#pizza").submit(function(event) {
+  //
+  //   var quantity = parseInt($("input#quantity").val());
+  //   var topping = parseInt($("input#topping").val());
+  //   var size = parseInt($("input#size").val());
+  //   var result = pizza("quantity", "topping", "size");
+  //
+  //   var total = "topping" + " " + "size" * quantity;
+  // })
+  <head><title>Pizza Place or Whatever</title>
+    <script>
+      function makeorder()
+      {
+        document.pizzaform.ordertextarea.value="A Test Of Some Description";
+      }
+    </script>
+  </head>
+})
--- spec/specs.js
+++ spec/specs.js
@@ -7,4 +7,13 @@ describe('pizza', function() {
     it("it determines the cost of pepperoni", function() {
       expect(pizza("pepperoni")).to.equal("$.50");
     });
+
+    // <head><title>Pizza Place or Whatever</title>
+    //   <script>
+    //     function makeorder()
+    //     {
+    //       document.pizzaform.ordertextarea.value="A Test Of Some Description";
+    //     }
+    //   </script>
+    // </head>
 });
