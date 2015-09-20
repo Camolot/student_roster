f31d84303fb83d76592a57b7619a0225f4e83d4a
william swanson <student@epicodus.com>
2015-09-20 11:24:12 -0700

    initial commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,15 @@
 $(document).ready(function() {
-  // $("form#pizza").submit(function(event) {
-  //
-  //   var quantity = parseInt($("input#quantity").val());
-  //   var topping = parseInt($("input#topping").val());
-  //   var size = parseInt($("input#size").val());
-  //   var result = pizza("quantity", "topping", "size");
-  //
-  //   var total = "topping" + " " + "size" * quantity;
-  // })
-  <head><title>Pizza Place or Whatever</title>
-    <script>
-      function makeorder()
-      {
-        document.pizzaform.ordertextarea.value="A Test Of Some Description";
-      }
-    </script>
-  </head>
+  $("form#pizza").submit(function(event) {
+
+    var quantity = parseInt($("input#quantity").val());
+    var topping = parseInt($("input#topping").val());
+    var size = parseInt($("input#size").val());
+
+    var x = document.getElementById("myRadio").value;
+    var y = size;
+    var z = quantity;
+
+    var total = (x + y) * z;
+    document.getElementById("pizza").innerHTML = total;
+  })
 })
