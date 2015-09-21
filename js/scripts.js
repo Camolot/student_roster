60cf8d2d19f92aef2aefa0f2e5e8a85333195b06
william swanson <student@epicodus.com>
2015-09-21 12:30:48 -0700

    initial commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,3 @@
-var pizza = function(numeric) {
+var roster = function(string) {
 
-    // var topping = 1.5;
-    // var size = 3.5;
-    // var quantity = parseInt(document.getElementById('getQuantity'),value, 10);
-    // var quantity = 3;
-
-    $(document).ready(function() {
-      $("form#pizzaForm").submit(function(event) {
-        var topping = parseInt($("input#topping").val());
-        var size = parseInt($("input#size").val());
-        var quantity = parseInt($("input#quantity").val());
-        var total = (topping + size) * quantity;
-        var result = pizzaTotal(total);
-
-        $(".numeric").text(numeric);
-        if (!result) {
-          $(".not").text("not");
-        }
-
-        $("#result"),show();
-        event.preventDefault();
-      })
-    })
-
-    // var total = (topping + size) * quantity
-    return total;
 }
--- spec/specs.js
+++ spec/specs.js
@@ -1,14 +1,3 @@
-describe('pizza', function() {
+describe('roster', function() {
 
-    //quantity
-    //topping
-    //size
-
-    // it("it determines the cost of pepperoni", function() {
-    //   expect(pizza("pepperoni")).to.equal(1);
-    // });
-
-    it("it determines the total cost and returns the number", function() {
-      expect(pizza(1.5, 3.5, 3)).to.equal(15);
-    })
 });
